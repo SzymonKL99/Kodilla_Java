c13dcb27f4fb65ff7eadb9a425060a3b0c78731f
Szymon Kliszcz <szymon.kliszczz@gmail.com>
2023-06-09 22:22:34 +0200

    First commit

--- src/main/java/com/example/kodillacourse/FirstClass.java
+++ src/main/java/com/example/kodillacourse/FirstClass.java
@@ -1,4 +1,19 @@
 package com.example.kodillacourse;
-
+import java.util.HashSet;
+import java.util..Set;
 public class FirstClass {
+    public static void main(String[] args) {
+        Set<String> OrderNumberSet = new HashSet<>();
+
+        OrderNumberSet.add("OR-123");
+        OrderNumberSet.add("OR-341");
+        OrderNumberSet.add("OR-123");
+        OrderNumberSet.add("OR-221");
+        OrderNumberSet.add("OR-122");
+
+        for (String OrderNumber : OrderNumberSet) {
+            System.out.println("Order number: " + OrderNumber);
+        }
+    }
+}
 }
